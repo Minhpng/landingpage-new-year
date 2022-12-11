a45671c1b18f4c233463fc05700be602e6b7babe
Minhpng <92564285+Lightrose142@users.noreply.github.com>
2022-12-12 00:14:56 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 export default defineConfig({
-    base: "/landingpage-new-year/"
+    base: "/landingpage-new-year/",
+    resolve: {
+        alias: {
+            '~/assets': path.join(__dirname, './assets'),
+        }
+    },
 })
